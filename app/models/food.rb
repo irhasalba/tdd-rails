594b9011d42207460b7ad9bbddc0039b9a9162fe
irhasalba <irhasalba19@gmail.com>
2022-04-12 20:14:18 +0700

    add sorted array

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
     # validates :name,presence: true, uniqueness: true 
     validates :name,presence: true, uniqueness: true
+
+    def self.by_letter(letter)
+        where("name LIKE ?","#{letter}%").order(:name)
+    end
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -42,5 +42,31 @@ RSpec.describe Food, type: :model do
 
     expect(food2.errors[:name]).to include("has already been taken")
   end
-
 end
+
+RSpec.describe "self#by_letter" do
+      it 'should return a sorted array of result that match' do
+        food1 = Food.create(
+        name: "Nasi Uduk",
+        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+        price: 10000.0
+      )
+
+      food2 = Food.create(
+        name: "Kerak Telor",
+        description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
+        price: 8000.0
+      )
+
+      food3 = Food.create(
+        name: "Nasi Semur Jengkol",
+        description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
+        price: 8000.0
+      )
+
+
+      expect(Food.by_letter('N')).to eq([food3,food1])
+
+      end
+    
+  end
