753efcbe205764ac5f99773330224bf3b9f103cc
irhasalba <irhasalba19@gmail.com>
2022-04-12 19:58:57 +0700

    Add validation for Food, invalid with duplicate name

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ApplicationRecord
-    validates :name,presence: true 
+    # validates :name,presence: true, uniqueness: true 
+    validates :name,presence: true, uniqueness: true
 end
